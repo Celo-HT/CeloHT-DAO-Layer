4665a1c6e7614be755735a32bcdfd1de09beca50
CeloHT <celoht3@gmail.com>
2025-12-12 20:25:02 -0400

    Crear contracts/TimelockController.sol

---
+++ contracts/TimelockController.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+/**
+ * CeloHT Timelock wrapper - deploy with minDelay and proposers/executors set
+ */
+contract CeloHTTimelock is TimelockController {
+    constructor(
+        uint256 minDelay,
+        address[] memory proposers,
+        address[] memory executors
+    ) TimelockController(minDelay, proposers, executors) {}
+}
